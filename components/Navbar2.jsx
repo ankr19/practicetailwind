7dd0e979f9f9f7c8f696f6e7e51199adbe317ad1
anand kumar <ankm335@gmail.com>
2024-03-19 23:39:15 +0530

    ui: building

---
+++ components/Navbar2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar2() {
+  return (
+    <div className=' bg-black text-white flex justify-between px-[20px] h-[60px] items-center'>
+      <div className='font-poppins text-[20px]'>
+        Logo
+      </div>
+      <div className='font-poppins'>
+        Navbar
+      </div>
+      <div className='font-poppins'>
+        Login
+      </div>
+    </div>
+  )
+}
