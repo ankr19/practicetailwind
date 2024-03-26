6df7374e9d9efa2df95ede5c317bba0b7633b903
anand kumar <ankm335@gmail.com>
2024-03-26 22:41:35 +0530

    pages: added pages

---
+++ app/dashboard/page.js
@@ -0,0 +1,10 @@
+import Sidebar3 from '@/components/Sidebar3'
+import React from 'react'
+
+export default function page() {
+  return (
+    <Sidebar3>
+      Dashboard
+    </Sidebar3>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Navbar from "@/components/Navbar";
 import Navbar2 from "@/components/Navbar2";
+import Sidebar2 from "@/components/Sidebar2";
+import Sidebar3 from "@/components/Sidebar3";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <Navbar2/>
+    <Sidebar3>
+      Home
+    </Sidebar3>
     </>
   );
 }
