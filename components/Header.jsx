7fe96ac2361afbb89afc6768357fb73624f7c3ca
anand kumar <ankm335@gmail.com>
2024-03-26 22:41:21 +0530

    comp: updated for showing url name

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,30 @@
+"use client"
+import { usePathname } from "next/navigation";
 import React from "react";
 import { BiMenu } from "react-icons/bi";
 export default function Header({ toggleSidebar }) {
+  const [head, setHeader] = React.useState("")
+  let location = usePathname();
+  React.useEffect(()=>{
+    console.log(location.replace("/",""))
+    setHeader(location.replace("/",""))
+  },[])
   return (
     <div className="flex justify-between items-center bg-black px-3 py-2 ml-[0px] lg:ml-[60px]">
-      <div className="">
+      <div className="flex justify-start items-center">
         <button
           data-drawer-target="sidebar-multi-level-sidebar"
           data-drawer-toggle="sidebar-multi-level-sidebar"
           aria-controls="sidebar-multi-level-sidebar"
           type="button"
           onClick={toggleSidebar}
-          className="text-white rounded-md border-[1px] py-1 px-2 items-center border-white sm:hidden"
+          className="text-white rounded-md border-[1px] py-1 px-2 items-center border-white sm:hidden mr-[5px]"
         >
           <BiMenu className="w-6 h-6" />
         </button>
+        <div className="text-white capitalize">
+          {head==="" ? "Home" : head}
+        </div>
       </div>
     </div>
   );
