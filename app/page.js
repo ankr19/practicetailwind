9e0968371f25c9795a630fcda6b8c99d554a8e55
anand kumar <ankm335@gmail.com>
2024-04-02 19:18:19 +0530

    build: fixes and created the product page

--- app/page.js
+++ app/page.js
@@ -1,15 +1,24 @@
-import Navbar from "@/components/Navbar";
-import Navbar2 from "@/components/Navbar2";
-import Sidebar2 from "@/components/Sidebar2";
-import Sidebar3 from "@/components/Sidebar3";
+"use client"
 import Sidebar4 from "@/components/Sidebar4";
-import Image from "next/image";
+import { Button, ButtonGroup, Card } from "@material-tailwind/react";
+let colors= [
+  { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
+  { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
+  { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Home() {
   return (
     <>
     <Sidebar4>
-      Home
+      <ButtonGroup >
+        
+        <Button></Button>
+      </ButtonGroup>
     </Sidebar4>
     </>
   );
